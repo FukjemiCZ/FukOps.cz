947f4a4daa64776eeb104c0479aca77a0da93ff5
FukjemiCZ <github@fukjemi.cz>
2023-07-23 12:50:56 +0200

    First

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -55,7 +55,7 @@ export default defineUserConfig({
     sidebar: {
       "/songs": [
         {
-          text: "Songs",
+          text: "Blogs",
           children: songFiles,
         },
       ],
